462d57399cd7e6825d5755b71696dab592813779
minhazapon <minhajulabedinapon@gmail.com>
2024-10-12 09:58:53 -0700

    first commit

---
+++ index.js
@@ -0,0 +1,12 @@
+"use strict";
+const country = 'khilfat jheniadah inshallah';
+console.log(country);
+const n = 2;
+const a = 2;
+console.log(n + a);
+const aponTech = ['javaScript', 'typeScript', 'reactjs', 'nodejs', 'mongodb', 'mongoose'];
+console.log(aponTech);
+const nz = 2000;
+const az = 5000;
+const zan = az + nz;
+console.log(zan);
